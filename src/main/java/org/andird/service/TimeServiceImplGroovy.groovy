c5e0d02a0d144c12fa0ab54181a28498a8c8b1d8
Andi Rustandi Djunaedi <a.djunaedi@netbiscuits.com>
2013-03-14 14:32:01 +0800

    Add groovy implementation of service

---
+++ src/main/java/org/andird/service/TimeServiceImplGroovy.groovy
@@ -0,0 +1,13 @@
+package org.andird.service
+
+import java.util.Locale
+
+class TimeServiceImplGroovy implements TimeService {
+
+    @Override
+    public String getCurrentTime(Locale locale) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
