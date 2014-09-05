30c7ab124dd2aa118e2ae3202c41e2ef1ac8d58a
Andi R Djunaedi <andi.r.djunaedi@gdn-commerce.com>
2014-09-05 10:10:01 +0700

    use spring-boot

--- src/test/java/org/andird/controller/HomeControllerTest.java
+++ src/test/java/org/andird/controller/HomeControllerTest.java
@@ -1,5 +1,19 @@
 package org.andird.controller;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.test.web.ModelAndViewAssert.assertModelAttributeValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import java.util.Locale;
+
 import org.andird.service.TimeService;
 import org.junit.After;
 import org.junit.Before;
@@ -10,77 +24,63 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Locale;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.initMocks;
-import static org.springframework.test.web.ModelAndViewAssert.assertModelAttributeValue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-
 public class HomeControllerTest {
 
-    private static final String SERVER_TIME = "serverTime";
-    private static final String PATH = "/";
-    private static final String HOME = "home";
-    private static final Locale ENGLISH = Locale.ENGLISH;
-    private static final String EN_RESULT = "en-result";
+  private static final String SERVER_TIME = "serverTime";
+  private static final String PATH = "/";
+  private static final String HOME = "home";
+  private static final Locale ENGLISH = Locale.ENGLISH;
+  private static final String EN_RESULT = "en-result";
 
-    @InjectMocks
-    private HomeController homeController;
+  @InjectMocks
+  private HomeController homeController;
 
-    @Mock
-    private TimeService timeService;
+  @Mock
+  private TimeService timeService;
 
-    private MockMvc mockMvc;
+  private MockMvc mockMvc;
 
-    private void mockCollaborators() {
-        when(this.timeService.getCurrentTime(HomeControllerTest.ENGLISH))
-                .thenReturn(HomeControllerTest.EN_RESULT);
-    }
+  @After
+  public void after() {
+    verifyNoMoreInteractions(this.timeService);
+  }
 
-    @Before
-    public void before() {
-        initMocks(this);
-        this.mockCollaborators();
-        this.mockMvc = standaloneSetup(this.homeController).build();
-    }
+  @Before
+  public void before() {
+    initMocks(this);
+    this.mockCollaborators();
+    this.mockMvc = standaloneSetup(this.homeController).build();
+  }
 
-    @After
-    public void after() {
-        verifyNoMoreInteractions(this.timeService);
-    }
+  @Test
+  public void homeAssertMavTest() throws Exception {
+    // note: we don't call the controller method manually here!
+    MvcResult mvcResult = this.mockMvc.perform(get(HomeControllerTest.PATH)).andReturn();
 
-    @Test
-    public void homeTest() throws Exception {
-        // note: we don't call the controller method manually here!
-        this.mockMvc.perform(get(HomeControllerTest.PATH)).andExpect(
-                status().isOk());
+    verify(this.timeService).getCurrentTime(HomeControllerTest.ENGLISH);
 
-        verify(this.timeService).getCurrentTime(HomeControllerTest.ENGLISH);
-    }
+    ModelAndView modelAndView = mvcResult.getModelAndView();
 
-    @Test
-    public void homeAssertMavTest() throws Exception {
-        // note: we don't call the controller method manually here!
-        MvcResult mvcResult = this.mockMvc
-                .perform(get(HomeControllerTest.PATH)).andReturn();
+    // assertions
+    assertThat(modelAndView, notNullValue());
+    assertThat(modelAndView.getViewName(), equalTo(HomeControllerTest.HOME));
+    assertThat(modelAndView.getModel().size(), equalTo(1));
 
-        verify(this.timeService).getCurrentTime(HomeControllerTest.ENGLISH);
+    // spring mvc assertions
+    assertModelAttributeValue(modelAndView, HomeControllerTest.SERVER_TIME,
+        HomeControllerTest.EN_RESULT);
+  }
 
-        ModelAndView modelAndView = mvcResult.getModelAndView();
+  @Test
+  public void homeTest() throws Exception {
+    // note: we don't call the controller method manually here!
+    this.mockMvc.perform(get(HomeControllerTest.PATH)).andExpect(status().isOk());
 
-        // assertions
-        assertThat(modelAndView, notNullValue());
-        assertThat(modelAndView.getViewName(), equalTo(HomeControllerTest.HOME));
-        assertThat(modelAndView.getModel().size(), equalTo(1));
+    verify(this.timeService).getCurrentTime(HomeControllerTest.ENGLISH);
+  }
 
-        // spring mvc assertions
-        assertModelAttributeValue(modelAndView, HomeControllerTest.SERVER_TIME,
-                HomeControllerTest.EN_RESULT);
-    }
+  private void mockCollaborators() {
+    when(this.timeService.getCurrentTime(HomeControllerTest.ENGLISH)).thenReturn(
+        HomeControllerTest.EN_RESULT);
+  }
 }
