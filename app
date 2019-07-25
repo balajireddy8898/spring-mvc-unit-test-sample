57df376215bc39cfdb5ac5649127ed9d012715e8
balajireddy8898 <46371857+balajireddy8898@users.noreply.github.com>
2019-07-25 14:55:12 +0000

    Update and rename app to app.bat

--- app
+++
@@ -1,2 +0,0 @@
-#del "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps\*.war"
-xcopy "C:\Program Files (x86)\Jenkins\workspace\banu7_master\target\spring-mvc-unit-test-sample-1.2.0-SNAPSHOT.war" "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps" 
---
+++ app.bat
@@ -0,0 +1,3 @@
+del "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps\spring-mvc-unit-test-sample-1.2.0-SNAPSHOT.war"
+xcopy "C:\Program Files (x86)\Jenkins\workspace\bhargav_master\target\spring-mvc-unit-test-sample-1.2.0-SNAPSHOT.war" 
+"C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps" 
